f1f2d9d23aa06dc029f46dee145922d59f2762b1
Roberta Bernardo <99690987+rlbernardo@users.noreply.github.com>
2022-05-18 15:39:57 -0300

    Add files via upload
    Project finalizado

--- pp-ts-pd.py
+++ pp-ts-pd.py
@@ -7,20 +7,40 @@ print('''Suas opções:
 [ 1 ] Papel
 [ 2 ] Tesoura''')
 jogador = int(input('Qual é a sua jogada?  '))
-print('-=_' * 10)
+print('-=_' * 15)
 print('Computador jogou {}'.format(itens[computador]))
 print('jogador jogou {}'.format(itens[jogador]))
-print('-=_' * 10)
+print('-=_' * 15)
 
 if computador == 0:
     if jogador == 0:
         print('Isso é um empate')
+    elif jogador == 1:
+        print('O jogador VENCE!')
     elif jogador == 2:
         print('O jogador VENCE!')
-    elif jogador
+    else:
+        print('Jogada inválida! Escolha uma opção válida')
 
-elif computador == 1:
+if computador == 1:
+    if jogador == 0:
+        print('Computador VENCE!')
+    elif jogador == 1:
+        print('Isso é um Empate!')
+    elif jogador == 2:
+        print('O jogador VENCE!')
+    else:
+        print('Jogada inválida! Escolha uma opção válida')
 
-elif computador == 2:
+if computador == 2:
+    if jogador == 0:
+        print('Computador PERDE!')
+    elif jogador == 1:
+        print('Computador VENCE!')
+    elif jogador == 2:
+        print('Isso é um EMPATE')
+    else:
+        print('Jogada inválida! Escolha uma opção válida')
 
+print('-=_' * 15)
 
