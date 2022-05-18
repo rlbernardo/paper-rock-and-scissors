b2542ed3acd7e42bc04713d707d2f9b8cf3f9896
Roberta Bernardo <99690987+rlbernardo@users.noreply.github.com>
2022-05-18 15:23:10 -0300

    Add files via upload
    criando com outro modulo e reestruturando condições

--- pp-ts-pd.py
+++ pp-ts-pd.py
@@ -1,6 +1,26 @@
-import random
+from random import randint
 
-def jogar():
-    usuário =input("'pd'para pedra, 'pp' para papel, 'ts' para tesoura")
-    computador - random.choice(['pd', 'pp', 'ts'])
+itens = ('pedra', 'papel', 'tesoura')
+computador = randint(0, 2)
+print('''Suas opções:
+[ 0 ] Pedra
+[ 1 ] Papel
+[ 2 ] Tesoura''')
+jogador = int(input('Qual é a sua jogada?  '))
+print('-=_' * 10)
+print('Computador jogou {}'.format(itens[computador]))
+print('jogador jogou {}'.format(itens[jogador]))
+print('-=_' * 10)
+
+if computador == 0:
+    if jogador == 0:
+        print('Isso é um empate')
+    elif jogador == 2:
+        print('O jogador VENCE!')
+    elif jogador
+
+elif computador == 1:
+
+elif computador == 2:
+
 
